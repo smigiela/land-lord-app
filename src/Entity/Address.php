07101b7c50c23ca4471d44242ddc2a4292b5cadc
Daniel Śmigiela <daniel.smigiela@outlook.com>
2024-03-04 21:38:25 +0100

    + address ORM model

---
+++ src/Entity/Address.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AddressRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AddressRepository::class)]
+class Address
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 150)]
+    private ?string $city = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $country = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $street = null;
+
+    #[ORM\Column(length: 50, nullable: true)]
+    private ?string $street_number = null;
+
+    #[ORM\Column(length: 50, nullable: true)]
+    private ?string $apartment_number = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): static
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getCountry(): ?string
+    {
+        return $this->country;
+    }
+
+    public function setCountry(string $country): static
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    public function getStreet(): ?string
+    {
+        return $this->street;
+    }
+
+    public function setStreet(?string $street): static
+    {
+        $this->street = $street;
+
+        return $this;
+    }
+
+    public function getStreetNumber(): ?string
+    {
+        return $this->street_number;
+    }
+
+    public function setStreetNumber(?string $street_number): static
+    {
+        $this->street_number = $street_number;
+
+        return $this;
+    }
+
+    public function getApartmentNumber(): ?string
+    {
+        return $this->apartment_number;
+    }
+
+    public function setApartmentNumber(?string $apartment_number): static
+    {
+        $this->apartment_number = $apartment_number;
+
+        return $this;
+    }
+}
